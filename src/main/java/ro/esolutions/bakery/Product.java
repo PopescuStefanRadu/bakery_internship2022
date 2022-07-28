861fc0d42529259f57ee62b909dffa9c64fa1072
Stefan Popescu <stefan.popescu@esolutions.ro>
2022-07-28 11:11:41 +0300

    Add delete method for product resource

--- src/main/java/ro/esolutions/bakery/Product.java
+++ src/main/java/ro/esolutions/bakery/Product.java
@@ -1,6 +1,7 @@
 package ro.esolutions.bakery;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,41 +10,9 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
+@Builder
 public class Product {
     final String id;
     final String name;
     final BigDecimal price;
-
-    static class Builder {
-        String id;
-        String name;
-        BigDecimal price;
-
-        private Builder() {}
-
-        public Builder withId(String id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder withName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder withPrice(BigDecimal price) {
-            this.price = price;
-            return this;
-        }
-
-        public Product build() {
-            return new Product(this.id, this.name, this.price);
-        }
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
 }
--- src/main/java/ro/esolutions/bakery/ProductsController.java
+++ src/main/java/ro/esolutions/bakery/ProductsController.java
@@ -1,34 +1,53 @@
 package ro.esolutions.bakery;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.http.ResponseEntity.badRequest;
-
 @RestController
-@RequestMapping("/products")
 public class ProductsController {
 
-    @GetMapping
-    public ResponseEntity<List<Product>> GetProducts() {
-//        return ResponseEntity.status(HttpStatus.CREATED).body(null);
-        return ResponseEntity.ok(
-                List.of(
-                        Product.builder()
-                                .withPrice(new BigDecimal("3"))
-                                .withName("Paine Franzela 2.5Lei")
-                                .withId(UUID.randomUUID().toString())
-                                .build())
-        );
+    private final List<Product> shopProducts = new ArrayList<>();
+
+
+    public ProductsController() {
+        shopProducts.add(Product.builder()
+                .price(new BigDecimal("3"))
+                .name("Paine Franzela 2.5Lei")
+                .id(UUID.randomUUID().toString())
+                .build());
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> GetAll() {
+        return ResponseEntity.ok(shopProducts);
+    }
+
+    @PostMapping(path = "/product/create")
+    public ResponseEntity<Object> Create(@RequestBody Product model) {
+        Product newProduct = Product.builder()
+                .id(UUID.randomUUID().toString())
+                .price(model.getPrice())
+                .name(model.getName())
+                .build();
+        shopProducts.add(newProduct);
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Product> Delete(@PathVariable("id") String id){
+        Optional<Product> removedProduct = shopProducts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        removedProduct.ifPresent(shopProducts::remove);
+        if (removedProduct.isPresent()) {
+            return ResponseEntity.ok(removedProduct.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
